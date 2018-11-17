2929c985062174888b2fc8ca6312be48409106c6
Dan Guest <dguest@cern.ch>
2018-11-18 00:47:22 +0100

    Updates to get git-atful working

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
-#!/usr/bin/env bash
+# set up some bash tools
 
 (
     cd ~
     ln -s **/ssh .ssh
     ln -s **/.gitconfig
 )
-PATH+=:~/git-atful
+PATH+=:~/**/git-atful
+cat <<EOF >> ~/.bashrc
 . ~/release_setup.sh
+. ~/**/git-atful/git-atful.sh
+EOF
