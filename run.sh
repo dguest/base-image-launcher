04a1205d00562921d5b895a332dc0c947b8122ae
Dan Guest <dguest@cern.ch>
2018-11-17 12:30:29 +0100

    More fixes

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-docker run -v ${PWD}:/home/atlas/portal -it atlas/analysisbase:21.2.51
+docker run --rm -it \
+       -v ${PWD}:/home/atlas/portal \
+       -w /home/atlas/portal/code \
+       atlas/analysisbase:21.2.51 \
+       bash -c "../setup.sh; bash"
--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
-ln -s **/ssh .ssh
-ln -s **/.gitconfig
-. release_setup.sh
+#!/usr/bin/env bash
+
+(
+    cd ~
+    ln -s **/ssh .ssh
+    ln -s **/.gitconfig
+)
+. ~/release_setup.sh
