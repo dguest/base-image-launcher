ff3eb8d1cfafd4fe7bbc33b203b8633d7201a3d8
Dan Guest <dguest@cern.ch>
2018-11-25 15:16:09 +0100

    Updates

--- setup.sh
+++ setup.sh
@@ -5,7 +5,11 @@
     ln -s **/ssh .ssh
     ln -s **/.gitconfig
 )
+touch .WORKING_DIRECTORY_STACK
 cat <<EOF >> ~/.bashrc
 . ~/release_setup.sh
 . ~/**/dans-awesome-pack/setup.sh
+DIRECTORY_STACK_FILE=~/**/.WORKING_DIRECTORY_STACK
 EOF
+echo "here is ${PWD}"
+echo save-dir-stack > ~/.bash_logout
