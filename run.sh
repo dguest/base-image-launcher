1e20ad2cf38ec23409ae33216599b026c3cdc1b1
Dan Guest <dguest@cern.ch>
2018-12-15 14:46:35 +0100

    Get the directory stack working

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ docker run --rm -it \
        -v ${PWD}:/home/atlas/portal \
        -w /home/atlas/portal/code \
        atlas/analysisbase:21.2.56 \
-       bash -c "../setup.sh; bash"
+       bash -c "../setup.sh; bash --login"
--- setup.sh
+++ setup.sh
@@ -9,7 +9,9 @@ touch .WORKING_DIRECTORY_STACK
 cat <<EOF >> ~/.bashrc
 . ~/release_setup.sh
 . ~/**/dans-awesome-pack/setup.sh
-DIRECTORY_STACK_FILE=~/**/.WORKING_DIRECTORY_STACK
+DIRECTORY_STACK_FILE=~/portal/code/.WORKING_DIRECTORY_STACK
+load-dir-stack
 EOF
 echo "here is ${PWD}"
 echo save-dir-stack > ~/.bash_logout
+
