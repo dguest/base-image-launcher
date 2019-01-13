a1a569362c571fcd02b0690bad4713f503880839
Cerny McCernface <cern@cern.ch>
2019-01-13 18:15:41 +0100

    updates

--- setup.sh
+++ setup.sh
@@ -1,15 +1,41 @@
 # set up some bash tools
 
+# name of the local directory that we mount wihin the image so that
+# some things are saved
+PORTAL=portal
+
+# set up the ssh and git config files
 (
     cd ~
-    ln -s **/ssh .ssh
-    ln -s **/.gitconfig
+    ln -s ${PORTAL}/ssh .ssh
+    ln -s ${PORTAL}/.gitconfig
 )
+
+# set up some useful utilities
+(
+    PATH=${PATH}:${HOME}/${PORTAL}/bin
+    if [[ ! -d ~/${PORTAL}/bin ]] ; then
+        mkdir ~/${PORTAL}/bin
+    fi
+    cd ~/${PORTAL}/bin
+    if ! type jq &> /dev/null ; then
+        JQ_URL=https://github.com/stedolan/jq/releases/download/jq-1.6/
+        JQ_VERS=jq-linux64
+        echo "getting jq"
+        wget ${JQ_URL}/${JQ_VERS}
+        mv ${JQ_VERS} jq
+        chmod +x jq
+    fi
+)
+
+
+# set up something to remember the directory stack between sessions
 touch .WORKING_DIRECTORY_STACK
 cat <<EOF >> ~/.bashrc
 . ~/release_setup.sh
-. ~/**/dans-awesome-pack/setup.sh
-DIRECTORY_STACK_FILE=~/portal/code/.WORKING_DIRECTORY_STACK
+. ~/${PORTAL}/dans-awesome-pack/setup.sh
+PATH=${PATH}:${HOME}/${PORTAL}/bin
+DIRECTORY_STACK_FILE=~/${PORTAL}/code/.WORKING_DIRECTORY_STACK
 load-dir-stack
 EOF
 echo "here is ${PWD}"
