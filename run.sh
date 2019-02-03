ea39cf52ed21b26b2b8193bad42806fe4354e852
Dan Guest <dguest@cern.ch>
2019-02-03 09:42:41 +0100

    bump package version number

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 docker run --rm -it \
        -v ${PWD}:/home/atlas/portal \
        -w /home/atlas/portal/code \
-       atlas/analysisbase:21.2.59 \
+       atlas/analysisbase:21.2.61 \
        bash -c "../setup.sh; bash --login"
