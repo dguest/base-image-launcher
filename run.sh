d1cca8a0192fbcdb1db6c5697fc0dd5d4e599442
Dan Guest <dguest@cern.ch>
2019-07-15 20:07:48 +0200

    give some options for release setup

--- run.sh
+++ run.sh
@@ -1,7 +1,43 @@
 #!/usr/bin/env bash
 
+# this should not be sourced
+if [[ $- == *i* ]] ; then
+    echo "Don't source me!" >&2
+    return 1
+else
+    # set the shell to exit if there's an error (-e), and to error if
+    # there's an unset variable (-u)
+    set -eu
+fi
+
+_usage() {
+   cat <<EOF
+usage: ${0##*/} [release]
+
+The "release" argument is required if not set already.
+Must be: 'top' or 'base'
+EOF
+}
+
+REL_FILE=RELEASE
+
+if [[ $# == 1 ]] ; then
+    TAG=21.2.82
+    case $1 in
+        top) RELEASE=analysistop:${TAG} ;;
+        base) RELEASE=analysisbase:${TAG} ;;
+        *) _usage; exit 1 ;;
+    esac
+    echo $RELEASE > $REL_FILE
+elif [[ ! -f $REL_FILE ]]; then
+    _usage
+    exit 1
+else
+    RELEASE=$(cat $REL_FILE)
+fi
+
 docker run --rm -it \
        -v ${PWD}:/home/atlas/portal \
        -w /home/atlas/portal/code \
-       atlas/analysisbase:21.2.77 \
+       atlas/${RELEASE} \
        bash -c "../setup.sh; bash --login"
